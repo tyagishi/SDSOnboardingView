61e787efcb567fa12191ff132262c30dbf7d6967
Tomoaki Yagishita <tyagishi@gmail.com>
2021-05-26 10:55:32 +0900

    initial version

---
+++ Sources/SDSOnboardingView/SDSOnboardingPages.swift
@@ -0,0 +1,78 @@
+//
+//  SwiftUIView.swift
+//
+//  Created by : Tomoaki Yagishita on 2021/05/25
+//  © 2021  SmallDeskSoftware
+//
+
+import SwiftUI
+import SwiftyUserDefaults
+
+public class SDSOnboardingPages: ObservableObject {
+    @Published public var shownPageNames = Defaults[\.shownPageNames]
+    @Published public var introPages:[SDSOnboardingPage]
+    
+    public init(_ introViewInfoList: [SDSOnboardingPage]) {
+        self.introPages = introViewInfoList
+    }
+    
+    public func addIntroPage(_ newPage: SDSOnboardingPage) {
+        self.introPages.append(newPage)
+    }
+    
+    public func resetShownPageInfo() {
+        shownPageNames = []
+        Defaults[\.shownPageNames] = shownPageNames
+    }
+    
+    public func storeShownPageInfo() {
+        shownPageNames = introPages.map{$0.id}
+        Defaults[\.shownPageNames] = shownPageNames
+    }
+    
+    public func findNotShownPageIDs() -> [String] {
+        var result:[String] = []
+        for page in introPages {
+            if !shownPageNames.contains(page.id) {
+                result.append(page.id)
+            }
+        }
+        if result.count == 0 {
+            result.append(introPages[0].id)
+        }
+        return result
+    }
+    
+    public func firstPageId() -> String {
+        guard introPages.count > 0 else { return "" }
+        return introPages[0].id
+    }
+    
+    public func findNextPageID(of id: String ) -> String {
+        if id == "" { return firstPageId() }
+        guard let index = introPages.firstIndex(where: {$0.id == id}) else { return firstPageId() }
+        let nextIndex = (index+1) < introPages.count ? index+1 : introPages.count-1
+        return introPages[nextIndex].id
+    }
+    
+    public func findPrevPageID(of id: String) -> String {
+        if id == "" { return firstPageId() }
+        guard let index = introPages.firstIndex(where: {$0.id == id}) else { return firstPageId() }
+        let prevIndex = index > 0 ? index-1 : 0
+        return introPages[prevIndex].id
+    }
+}
+
+extension DefaultsKeys {
+    var shownPageNames:DefaultsKey<[String]> { .init("shownPageNames", defaultValue: [])}
+}
+
+public struct SDSOnboardingPage : Identifiable {
+    public let id: String // page name, should be unique
+    let content: () -> AnyView
+    
+    public init(name: String, @ViewBuilder content: @escaping () -> AnyView) {
+        self.id = name
+        self.content = content
+    }
+}
---
+++ Sources/SDSOnboardingView/SDSOnboardingView.swift
@@ -0,0 +1,106 @@
+//
+//  SwiftUIView.swift
+//
+//  Created by : Tomoaki Yagishita on 2021/05/25
+//  © 2021  SmallDeskSoftware
+//
+
+import SwiftUI
+
+public struct SDSOnboardingView: View {
+    @Binding var present:Bool
+    @ObservedObject var introPages: SDSOnboardingPages
+    let viewSize:CGSize // valid only for macOS
+    
+    public init(isPresented: Binding<Bool>, _ introPages: SDSOnboardingPages,_ viewSize: CGSize = .zero) {
+        #if os(iOS)
+        UIPageControl.appearance().currentPageIndicatorTintColor = .black
+        UIPageControl.appearance().pageIndicatorTintColor = UIColor.black.withAlphaComponent(0.3)
+        #endif
+        self._present = isPresented
+        self.introPages = introPages
+        self.viewSize = viewSize
+    }
+    
+    public var body: some View {
+        ZStack {
+            #if os(iOS)
+            LocalTabView(introPages)
+                .tabViewStyle(PageTabViewStyle())
+            #elseif os(macOS)
+            LocalTabView(introPages)
+                .tabViewStyle(DefaultTabViewStyle())
+                .frame(width: viewSize.width, height: viewSize.height)
+            #endif
+            VStack { // for later/close button
+                Spacer()
+                HStack {
+                    Button("later") {
+                        // user can come back to same intro
+                        present.toggle()
+                    }
+                    .padding(.leading, 30)
+                    Spacer()
+                    Button("close") {
+                        present.toggle()
+                        introPages.storeShownPageInfo()
+                    }
+                    .padding(.trailing, 20)
+                }
+                .padding(.bottom, 10)
+            }
+        }
+        .onAppear {
+            if introPages.introPages.count == 0 {
+                present = false
+            }
+        }
+    }
+}
+
+struct LocalTabView: View {
+    let introPages: SDSOnboardingPages
+    @State var selectedPageID: String
+    
+    init(_ pages: SDSOnboardingPages) {
+        self.introPages = pages
+        _selectedPageID = State(wrappedValue: introPages.findNotShownPageIDs().first!)
+    }
+    var body: some View {
+        ZStack {
+            TabView(selection: $selectedPageID) {
+                ForEach(introPages.introPages) { viewInfo in
+                    viewInfo.content()
+                        .tabItem { Text(viewInfo.id) }
+                        .tag(viewInfo.id)
+                }
+            }
+            #if os(macOS)
+            HStack { // prev/next button
+                Image(systemName: "arrow.left.circle")
+                    .resizable().scaledToFit()
+                    .frame(width: 50, height: 50)
+                    .opacity(0.5)
+                    .onTapGesture {
+                        selectedPageID = introPages.findPrevPageID(of: selectedPageID)
+                    }
+                Spacer()
+                Image(systemName: "arrow.right.circle")
+                    .resizable().scaledToFit()
+                    .frame(width: 50, height: 50)
+                    .opacity(0.5)
+                    .onTapGesture {
+                        selectedPageID = introPages.findNextPageID(of: selectedPageID)
+                    }
+            }
+            #endif
+        }
+    }
+    
+}
+
+struct SDSOnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SDSOnboardingView(isPresented: .constant(true),  SDSOnboardingPages([SDSOnboardingPage(name: "view1", content: { AnyView(Text("test"))})]))
+    }
+}
